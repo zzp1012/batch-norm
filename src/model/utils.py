a0174546bf532d08e9fc4f764f988f8908aa82be
zzp1012 <zzp1012@sjtu.edu.cn>
2022-04-27 13:03:11 +0800

    update: use 0.05 and use E[x^2]

--- src/model/utils.py
+++ src/model/utils.py
@@ -6,7 +6,7 @@ class BatchNorm2d(nn.Module):
     def __init__(self,
                  num_features: int,
                  eps: float = 1e-5,
-                 momentum: float = 0.1,
+                 momentum: float = 0.05,
                  device=None,
                  dtype=None) -> None:
         factory_kwargs = {'device': device, 'dtype': dtype}
@@ -26,18 +26,19 @@ class BatchNorm2d(nn.Module):
                 input: torch.Tensor)-> torch.Tensor:
         if self.training:
             batch_mean = input.mean((0, 2, 3), keepdim = True).detach()
-            batch_var = input.var((0, 2, 3), unbiased = False, keepdim = True).detach()
-            mean_bn = batch_mean
-            var_bn = batch_var
-            self.running_mean.mul_(1 - self.momentum)
-            self.running_mean.add_(self.momentum * batch_mean.data.flatten())
-            self.running_var.mul_(1 - self.momentum)
-            self.running_var.add_(self.momentum * batch_var.data.flatten())
+            batch_var = (input ** 2).mean((0, 2, 3), keepdim = True).detach()
+            if self.num_batches_tracked == 0:
+                self.running_mean.copy_(batch_mean.data.flatten())
+                self.running_var.copy_(batch_var.data.flatten())
+            else:
+                self.running_mean.mul_(1 - self.momentum)
+                self.running_mean.add_(self.momentum * batch_mean.data.flatten())
+                self.running_var.mul_(1 - self.momentum)
+                self.running_var.add_(self.momentum * batch_var.data.flatten())
             self.num_batches_tracked += 1
-        else:
-            mean_bn = torch.autograd.Variable(self.running_mean).reshape(1, self.num_features, 1, 1)
-            var_bn = torch.autograd.Variable(self.running_var).reshape(1, self.num_features, 1, 1)
-
+        mean_bn = torch.autograd.Variable(self.running_mean).reshape(1, self.num_features, 1, 1)
+        var_bn = torch.autograd.Variable(self.running_var).reshape(1, self.num_features, 1, 1)
+        var_bn = (var_bn - mean_bn ** 2).detach()
         weight = self.weight.reshape(1, self.num_features, 1, 1)
         bias = self.bias.reshape(1, self.num_features, 1, 1)
         input_normalized = (input - mean_bn) / torch.sqrt(var_bn + self.eps)
