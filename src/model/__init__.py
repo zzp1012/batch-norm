8f61c89550b48da474ed055902de2cfc25cd90cc
zzp1012 <1012zzphh@gmail.com>
2022-05-10 19:57:44 +0800

    update: add the option for densenet

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -18,24 +18,19 @@ def prepare_model(model_name: str,
     """
     logger = get_logger(__name__)
     logger.info(f"prepare the {model_name} model for dataset {dataset}")
-    if model_name.startswith("vgg") and dataset.startswith("cifar"):
-        import model.cifar_vgg as cifar_vgg
-        if dataset == "cifar10":
-            num_classes = 10
-        elif dataset == "cifar100":
-            num_classes = 100
+    if dataset == "cifar10":
+        num_classes = 10
+        if model_name.startswith("vgg"):
+            import model.cifar_vgg as cifar_vgg
+            model = cifar_vgg.__dict__[model_name](num_classes=num_classes, bn_type=bn_type)
+        elif model_name.startswith("ResNet"):
+            import model.cifar_resnet as cifar_resnet
+            model = cifar_resnet.__dict__[model_name](num_classes=num_classes, bn_type=bn_type)
+        elif model_name == "densenet_cifar":
+            import model.cifar_densenet as cifar_densenet
+            model = cifar_densenet.densenet_cifar(bn_type=bn_type)
         else:
-            raise ValueError(f"{dataset} is not supported.")
-        model = cifar_vgg.__dict__[model_name](num_classes=num_classes, bn_type=bn_type)
-    elif model_name.startswith("ResNet") and dataset.startswith("cifar10"):
-        import model.cifar_resnet as cifar_resnet
-        if dataset == "cifar10":
-            num_classes = 10
-        elif dataset == "cifar100":
-            num_classes = 100
-        else:
-            raise ValueError(f"{dataset} is not supported.")
-        model = cifar_resnet.__dict__[model_name](num_classes=num_classes, bn_type=bn_type)
+            raise ValueError(f"unknown model name: {model_name} for dataset {dataset}")
     else:
-        raise ValueError(f"unknown model name: {model_name}")
+        raise ValueError(f"{dataset} is not supported.")
     return model
