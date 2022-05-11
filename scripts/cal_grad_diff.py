ffa6a123c8a9acdeb6dcc9dcd782186fae2bc82c
eezqs <eezqs@login1.pi.sjtu.edu.cn>
2022-05-11 14:11:47 +0800

    update: minor update

--- scripts/cal_grad_diff.py
+++ scripts/cal_grad_diff.py
@@ -2,16 +2,20 @@ import torch
 import pandas as pd
 
 # set the global path
-loss1_grad_path = "/data2/zzp1012/batch-norm-exp2/outs/0508/0508-144043-seed0-cifar10-vgg11-loss1-bs128/exp2/classifier.0_grads.pt"
-loss2_grad_path = "/data2/zzp1012/batch-norm-exp2/outs/0508/0508-144115-seed0-cifar10-vgg11-loss2-bs128/exp2/classifier.0_grads.pt"
-loss3_grad_path = "/data2/zzp1012/batch-norm-exp2/outs/0508/0508-144147-seed0-cifar10-vgg11-loss3-bs128/exp2/classifier.0_grads.pt"
-loss4_grad_path = "/data2/zzp1012/batch-norm-exp2/outs/0508/0508-144220-seed0-cifar10-vgg11-loss4-bs128/exp2/classifier.0_grads.pt"
+loss1_grad_path = "/lustre/home/acct-eezqs/eezqs/zzp1012/batch-norm-exp2/outs/0511/loss1/0511-134717-seed0-cifar10-vgg11-loss1-bs128/exp2/classifier.0_grads.pt"
+loss2_grad_path = "/lustre/home/acct-eezqs/eezqs/zzp1012/batch-norm-exp2/outs/0511/loss2/0511-140218-seed0-cifar10-vgg11-loss2-bs128/exp2/classifier.0_grads.pt"
+loss3_grad_path = "/lustre/home/acct-eezqs/eezqs/zzp1012/batch-norm-exp2/outs/0511/loss3/0511-134849-seed0-cifar10-vgg11-loss3-bs128/exp2/classifier.0_grads.pt"
+loss4_grad_path = "/lustre/home/acct-eezqs/eezqs/zzp1012/batch-norm-exp2/outs/0511/loss4/0511-134935-seed0-cifar10-vgg11-loss4-bs128/exp2/classifier.0_grads.pt"
 
 # load the grad
 loss1_grad = torch.load(loss1_grad_path)
+print(loss1_grad.shape)
 loss2_grad = torch.load(loss2_grad_path)
+print(loss1_grad.shape)
 loss3_grad = torch.load(loss3_grad_path)
+print(loss1_grad.shape)
 loss4_grad = torch.load(loss4_grad_path)
+print(loss1_grad.shape)
 loss_grad_dict = {
     "loss1": loss1_grad,
     "loss2": loss2_grad,
@@ -27,8 +31,8 @@ for loss_type_1, grad_1 in loss_grad_dict.items():
     for loss_type_2, grad_2 in loss_grad_dict.items():
         print(f"col: {loss_type_2}")
         assert len(grad_2.shape) == 2, "the grad should be 2-dim"
-        grad_diff = torch.mean((grad_1 - grad_2) ** 2)  # (N,)
-        row.append(grad_diff.item())
+        grad_diff_norm = torch.norm(grad_1 - grad_2, p=2, dim=-1) / torch.norm(grad_1, p=2, dim=-1) # (N, )
+        row.append(torch.mean(grad_diff_norm).item())
     data.append(row)
 
 data_df = pd.DataFrame(data, index = loss_grad_dict.keys(), columns = loss_grad_dict.keys())
