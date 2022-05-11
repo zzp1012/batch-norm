7b769a509b0f741237636f7eb7055cb77085d8ef
zzp1012 <1012zzphh@gmail.com>
2022-05-11 22:21:00 +0800

    update: add BN in alexnet

--- src/model/alexnet.py
+++ src/model/alexnet.py
@@ -25,20 +25,25 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
         )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
+        self.before_bn = nn.Sequential(
+            nn.Dropout(inplace=False),
             nn.Linear(256 * 2 * 2, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.ReLU(inplace=False),
+        )
+        self.bn = nn.BatchNorm1d(4096, affine=False)
+        self.after_bn = nn.Sequential(
+            nn.Dropout(inplace=False),
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
             nn.Linear(4096, num_classes),
         )
 
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), 256 * 2 * 2)
-        x = self.classifier(x)
+        x = self.before_bn(x)
+        x = self.bn(x)
+        x = self.after_bn(x)
         return x
 
 if __name__ == "__main__":
