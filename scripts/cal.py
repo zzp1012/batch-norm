c9d0ad777987debbbea6e7a5b2ea550b6fcd1008
zzp1012 <1012zzphh@gmail.com>
2022-05-15 14:47:18 +0800

    update: remove the lenet5

--- scripts/cal.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-DATA_PATH = "NEED TO BE FILLED"
-
-data = pd.read_csv(DATA_PATH)
-diff = data["H_off_dot_Y_linear_norm"] - data["H_off_dot_Y_none_norm"]
-print("mean: ", diff.mean())
-print("std: ", diff.std())
-print(data.describe())
